e24f51f7df76ee8b699b48e8560bccb60cf60d61
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-14 21:06:26 -0400

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -3,7 +3,6 @@ module.exports = {
 	description: 'give stars to people',
 	cooldown: 5,
 	execute(message, args) {
-        if (await users.get(target.id) === undefined) {await users.set(target.id, 0)}
         async function donate() {
             let ask = parseInt(args[1]);
             let check = await users.get(message.author.id)
@@ -11,15 +10,14 @@ module.exports = {
                 message.channel.send("who u givin golden stars to");
             } else if (!ask || ask < 1 || ask > check) {
                 message.channel.send("thats not a valid number of golden stars to give")
-
             } else if (target.id === message.author.id) {
                 message.channel.send("bruh you cant give golden stars to yourself smh")
             } else if (target.user.bot) {
                 message.channel.send("bruh you cant give golden stars to a bot smh")
             } else {
-                let give = parseInt(args[1]);
-                addMoni(message.author.id, -give);
-                addMoni(targets.id, give);
+		if (await users.get(target.id) === undefined) {await users.set(target.id, 0)}
+                addMoni(message.author.id, -ask);
+                addMoni(targets.id, ask);
                 const balsoloEmbed = new Discord.MessageEmbed()
                     .setColor('#dd2de0')
                     .setTitle(message.author.username + ` donation to ` + target.displayName)
