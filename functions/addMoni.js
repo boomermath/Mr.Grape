170f194dd5c8f2787f11fbdcdd6568d2ae610cd2
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-15 09:33:57 -0400

    Update addMoni.js

--- functions/addMoni.js
+++ functions/addMoni.js
@@ -3,13 +3,13 @@ const Keyv = require('keyv');
 const users = new Keyv(process.env.DATABASE_URL, {
     namespace: 'users'
 });
+
 async function addMoni(who, howmuch) {
     let rightnow = await users.get(who);
     if (rightnow === undefined) {
         await users.set(who, 0)
     }
-    let moremoni = rightnow + howmuch;
-    await users.set(who, moremoni)
+    await users.set((rightnow+howmuch), moremoni)
 }
 
 module.exports.addMoni = addMoni;
