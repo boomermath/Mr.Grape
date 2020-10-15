236ccefb17c54f69c4d5a1dea13bf1e9ee5c3877
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-15 08:46:05 -0400

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,3 +1,9 @@
+
+const Discord = require('discord.js');
+const Keyv = require('keyv');
+const users = new Keyv(process.env.DATABASE_URL, {
+    namespace: 'users'
+});
 module.exports = {
 	name: 'bal',
 	description: 'check ur balance',
