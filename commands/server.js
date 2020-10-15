4066a8b49d1ddbeb75506e03eebb69688ad59b95
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-15 08:52:14 -0400

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,9 +1,5 @@
 
 const Discord = require('discord.js');
-const Keyv = require('keyv');
-const users = new Keyv(process.env.DATABASE_URL, {
-    namespace: 'users'
-});
 module.exports = {
 	name: 'server',
 	description: 'get some basic info about your server',
