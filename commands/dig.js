a61059965548a54f0a1be59e5323573bc733aa0b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-15 09:03:00 -0400

    Update dig.js

--- commands/dig.js
+++ commands/dig.js
@@ -4,6 +4,7 @@ const Keyv = require('keyv');
 const users = new Keyv(process.env.DATABASE_URL, {
     namespace: 'users'
 });
+const add = require('./functions/addMoni.js');
 module.exports = {
 	name: 'dig',
 	description: 'dig to earn stars',
@@ -21,6 +22,6 @@ module.exports = {
             .setTimestamp()
             .setFooter('Grape Enterprises');
         message.channel.send(mine);
-        addMoni(message.author.id, earn);
+        add.addMoni(message.author.id, earn);
 	}
 };
