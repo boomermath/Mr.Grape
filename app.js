5ab900a05a25f046d101c54d4795f1c4dd288c39
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-17 18:26:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
+const DisTube = require('distube'),
 const config  = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
