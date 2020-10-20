bd4ecd1c8e19d22a842ff6bd90addc440168cd5a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-20 12:32:49 -0400

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'play music',
 	cooldown: 0,
 	execute(message, args, d) {
-        if (!message.member.voice.channel) return message.channel.send( 'You must be in a voice channel!`)
+        if (!message.member.voice.channel) return message.channel.send( 'You must be in a voice channel!')
     if (!d.distube.isPlaying(message)) return message.channel.send('There is nothing playing!')
     d.distube.stop(message);
     message.channel.send('Done.')
