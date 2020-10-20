91dbf1109226f49df21c19ac5d5cfda57c3563bf
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-20 11:27:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const config  = require('./config.json');
+const config  = require('../config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const Keyv = require('keyv');
