64729b0038d08edb855be9e48d8c3f9141ae39d5
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-20 15:52:45 -0400

    Update t.js

--- commands/t.js
+++ commands/t.js
@@ -3,18 +3,22 @@ module.exports = {
 	description: 'dig to earn stars',
 	cooldown: 0,
 	execute(message, args, d) {
-    async function test() {
-    const arr = [23,24,25,26,27]
-    await d.items.set(message.author.id, arr);
-    }
-    async function test2() {
-    let gui = await d.items.get(message.author.id);
-    let ui = Array.from(gui);
-	   let gu = ui[0]
-    message.channel.send(gu);
-             message.channel.send(ui[3])
-    }
-    test();
-    test2();
+    async function buy() {
+	    const obj = {
+		apple: 1,
+		banana: 2
+	    }
+    let haveRaw = await d.items.set(message.author.id, obj);    
+    
+	    
+    }	    
+		async function reu () {
+		let somethin = await items.get(message.author.id);
+		message.channel.send(somethin.apple);
+		}
+		
+		
+		buy();
+		reu();
 	}
 };
