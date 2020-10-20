11854d7895330cb2a5e84d829f59cd51b910a2b3
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-20 14:10:17 -0400

    Create t.js

---
+++ commands/t.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'dig',
+	description: 'dig to earn stars',
+	cooldown: 0,
+	execute(message, args, d) {
+    async function test() {
+    const arr = [23,24,25,26,27]
+    await items.set(message.author.id, arr);
+    }
+    async function test2() {
+    let gu = await items.get(message.author.id);
+    message.channel.send(gu[0])
+       message.channel.send(gu[1])
+          message.channel.send(gu[2])
+             message.channel.send(gu[3])
+             
+    }
+    test();
+    test2();
+	}
+};
