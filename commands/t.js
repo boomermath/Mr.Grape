bd952164e7361e25612f6ae6cc2b11f8320edb53
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-20 14:11:26 -0400

    Update t.js

--- commands/t.js
+++ commands/t.js
@@ -5,10 +5,10 @@ module.exports = {
 	execute(message, args, d) {
     async function test() {
     const arr = [23,24,25,26,27]
-    await items.set(message.author.id, arr);
+    await d.items.set(message.author.id, arr);
     }
     async function test2() {
-    let gu = await items.get(message.author.id);
+    let gu = await d.items.get(message.author.id);
     message.channel.send(gu[0])
        message.channel.send(gu[1])
           message.channel.send(gu[2])
