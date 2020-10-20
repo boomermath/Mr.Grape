f7f0e6df888ea3dcbe8a8a6bd07530b16e8a0b82
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-20 19:33:13 -0400

    Update t.js

--- commands/t.js
+++ commands/t.js
@@ -18,17 +18,16 @@ async function buy() {
             await d.items.set(message.author.id, blankObj)
         }
         let regex = /\d+/g;
-        let numberOfItemsRaw = args.join(' ').match(regex);
-	let numberOfItems = parseInt(numberOfItemsRaw);
+        let numberOfItems = parseInt(args.join(' ').match(regex));
 	if (numberOfItems === undefined) {numberofItems = 1;}
 	if (numberOfItems === 0) {message.channel.send('ok karen'); return;}
         let item = args.join(' ').replace(numberOfItems, '').replace(' ', '');
         if (!Object.keys(itemCost).includes(item)) {message.channel.send("dude that's not an item in the shop"); return;}
-	let total = itemCost.item * parseInt(numberOfItems);
+	let total = itemCost.args.join(' ').replace(numberOfItems, '').replace(' ', '') * numberOfItems;
        if (total > await d.users.get(message.author.id)) {message.channel.send('you donut have enough money, rip'); return;}
        d.addMoni(message.author.id, -total)
-       if (have.`${item}` === undefined) {have.item = 0}
-	have.`${item}` += numberOfItems
+       if (have.args.join(' ').replace(numberOfItems, '').replace(' ', '') === undefined) {have.item = 0}
+	have.args.join(' ').replace(numberOfItems, '').replace(' ', '') += numberOfItems
 	d.items.set(message.author.id, have)
     }	    
   buy();
