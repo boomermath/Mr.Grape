a1e66eae154f5888bda70049bea5ae1e5a82b338
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-22 11:26:56 -0400

    Update and rename t.js to buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,7 +1,8 @@
 module.exports = {
 	name: 't',
+	aliases: ['purchase'],
 	description: 'dig to earn stars',
-	cooldown: 0,
+	cooldown: 2,
 	execute(message, args, d) {
 const blankObj = {}
 const itemCost = {
@@ -44,8 +45,20 @@ async function buy() {
     }
     have[item] += numberOfItems
     d.items.set(message.author.id, have);
-    message.channel.send(item);
-    message.channel.send(numberOfItems);
+    let receipt;
+    if (numberOfItems === 1) {receipt = "You successfully purchased a " + item;}
+    else {receipt = "You successfully purchased "+ numberOfItems + " " + item + "s";}
+	    const buy = new d.Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(message.author.username + "'s purchase")
+                .addFields({
+                    name: 'Receipt',
+                    value: receipt
+                }, )
+                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                .setTimestamp()
+                .setFooter('Grape Bank Inc.');
+            message.channel.send(buy);
 }
 buy();
 	}
