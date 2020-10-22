27abeb1525ba95746795abf4f0a1285dc2f4a035
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-22 13:34:39 -0400

    Update inv.js

--- commands/inv.js
+++ commands/inv.js
@@ -1,21 +1,46 @@
 module.exports = {
-	name: 'inv',
-	description: 'dig to earn stars',
-	aliases: ['inventory'],
-	cooldown: 3,
-	execute(message, args, d) {
-	    async function inv() {	
-            let inv = await d.items.get(message.author.id);
-	    const invEmbed = new d.Discord.MessageEmbed()
-                    .setColor('#dd2de0')
-                    .setTitle(message.author.username + "'s inventory")
-                    .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-                    .setTimestamp()
-                    .setFooter('Grape Storage Org.');
-		for (const key in inv) {invEmbed.addFields({name: key.charAt(0).toUpperCase() + key.slice(1) +"(s)", value: `${inv[key]}`});}
-		message.edit(invEmbed);
-                message.channel.send(invEmbed);
-	      }
-	  inv();
+    name: 'inv',
+    description: 'dig to earn stars',
+    aliases: ['inventory'],
+    cooldown: 3,
+    execute(message, args, d) {
+	let target = message.mentions.members.first();
+        async function inv() {
+	    let person;
+	    let personName;
+            if (args[0] === undefined) {
+                person = message.author;
+                personName = message.author.username;
+            } else if (args[0].startsWith("<@") && args[0].endsWith(">")) {
+                person = target;
+                personName = target.displayName;
+                if (target.user.bot) {
+                    message.channel.send('No bots in da economy! (except me cus im cool)');
+                    return;
+                }
+            } else {
+                message.channel.send('Use a valid mention!');
+                return;
+            }
+            let inv = await d.items.get(person.id);
+            const invEmbed = new d.Discord.MessageEmbed()
+                .setColor('#dd2de0')
+                .setTitle(personName + "'s inventory")
+                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                .setTimestamp()
+                .setFooter('Grape Storages Org.');
+            if (Object.entries(inv) === '') {invEmbed.addFields({name: 'There is nothing!', value: '_'});}
+	    else {
+            for (const key in inv) {
+                invEmbed.addFields({
+                    name: key.charAt(0).toUpperCase() + key.slice(1) + "(s)",
+                    value: `${inv[key]}`
+                });
+            }
 	}
+            message.edit(invEmbed);
+            message.channel.send(invEmbed);
+        }
+        inv();
+    }
 };
