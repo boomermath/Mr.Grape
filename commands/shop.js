dd1652205119e8dc1674afd00c896a33cb9f7f22
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-22 15:15:31 -0400

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -15,6 +15,7 @@ module.exports = {
 						{ name: 'carrotdetector', value: 'increases the chance you find a carrot in the carrot job\ncost: 50:star:'},
 						{ name: 'starmagnet', value: 'increases the chance you find :star:s\ncost: 100:star:'},
 						{ name: 'starmill', value: 'generates you :star:s passively at a rate of 1:star: per 10m\ncost: 400:star:'},
+						{ name: 'shovel', value: 'gives you more stars for digging job\ncost: 100:star:'},
 					)
 			                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
 					.setTimestamp()
