d02b4bd07b07c033481c200a506931204a89afda
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-22 15:25:51 -0400

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -4,15 +4,20 @@ module.exports = {
 	description: 'do honest work to get stars',
 	cooldown: 20,
 	execute(message, args, d) {
+	async function work() {
+	let inv = await d.items.get(message.author.id);
 	let earn = Math.round(Math.random() * 7) + 1;
         let chooseWork = Math.round(Math.random() * 2);
-        let ifEarn = Math.round(Math.random() * 1) + 1
+        const ifEarn = [1,2];
+	if (chooseWork === 0 && inv.hasOwnProperty('orangedetector')) {for (let i = 0; i < inv.orangedetector; i++) {ifEarn.push(1);}}
+	else if (chooseWork === 1 && inv.hasOwnProperty('mangodetector')) {for (let i = 0; i < inv.mangodetector; i++) {ifEarn.push(1);}}
+	else if (chooseWork === 2 && inv.hasOwnProperty('carrotdetector')) {for (let i = 0; i < inv.mangodetector; i++) {ifEarn.push(1);}}
         const situation = [
             ['Help Mr.Grape find his orange!', 'will you help me find my orange?\nit fell in a bush full of bananas over there, but i could not find it.\nPlease go there and find my orange.', 'Yay, you found my orange! Here, take ' + earn + ' :star:s!', "That's not my orange, that's a banana! Try again later."],
             ['Help Mr.Grape catch his mango!', 'I am trying to catch a flying mango, but it keeps disappearing.\nSo will you catch it and bring it to me?', 'Yay, you found my mango! Here, take ' + earn + ' :star:s!', "You didn't catch my mango? Too bad, try again next time"],
             ['Help Mr.Grape find his rabbit.', 'my pet rabbit has escaped!\nhe really like carrots\ncan you help lure him home?', 'Yay, you found my rabbit! Here, take ' + earn + ' :star:s!', "Sorry, I was asking for a carrot, not a lime."]
         ];
-
+      
         description = situation[chooseWork][0];
         background = situation[chooseWork][1];
         if (ifEarn === 1) {
@@ -21,7 +26,6 @@ module.exports = {
         } else {
             outcome = situation[chooseWork][3];
         }
-
         const job = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(message.author.username + `'s job`)
@@ -38,4 +42,5 @@ module.exports = {
 
         message.channel.send(job);
 	}
+    }
 };
