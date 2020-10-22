0652e60d6c402233ce73baa9f84fdd9fb85a9946
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-22 12:32:04 -0400

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,54 +1,45 @@
 module.exports = {
-	name: 'bal',
-	aliases: ['balance', 'wallet'],
-	description: 'check ur balance',
-	execute(message, args, d) {
-let target = message.mentions.members.first();
-let person;
-let personName;
-let argu = args[0];
-if (argu === undefined) {
-    person = message.author;
-    personName = message.author.username;
-}
-else if (argu.startsWith("<@") && argu.endsWith(">")) {
-    person = target;
-    personName = target.displayName;
-}
-else {message.channel.send('Use a valid mention!');}
+    name: 'bal',
+    aliases: ['balance', 'wallet'],
+    description: 'check ur balance',
+    execute(message, args, d) {
+        async function bal() {
+            let target = message.mentions.members.first();
+            let person;
+            let personName;
+            if (args[0] === undefined) {
+                person = message.author;
+                personName = message.author.username;
+            } else if (args[0].startsWith("<@") && args[0].endsWith(">")) {
+                person = target;
+                personName = target.displayName;
+		if (target.user.bot) {message.channel.send('No bots in da economy! (except me cus im cool)'); return;}
+            } else {
+                message.channel.send('Use a valid mention!');
+		return;
+            }
+            let bal = await d.users.get(person.id)
+	    let displayBal()
+            if (bal === null || bal === undefined) {
+                d.users.set(person.id, 0);
+		displayBal = 0
+	    } else {
+		displayBal = bal    
+	    }
+                const balEmbed = new d.Discord.MessageEmbed()
+                    .setColor('#dd2de0')
+                    .setTitle(personName + `'s balance`)
+                    .addFields({
+                        name: 'Balance',
+                        value: displayBal + " :star:s"
+                    }, )
+                    .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+                    .setTimestamp()
+                    .setFooter('Grape Bank Inc.');
+                message.channel.send(balEmbed);
 
-  async function bal() {    
-	  let bal = await d.users.get(person.id)
-	if (argu !== undefined && target.user.bot) {message.channel.send('No bot in da economy (except me!)'); return;}
-        if ( bal === null || bal === undefined) {
-            d.users.set(person.id, 0);
-            const balsolooEmbed = new d.Discord.MessageEmbed()
-                .setColor('#dd2de0')
-                .setTitle(personName + `'s balance`)
-                .addFields({
-                    name: 'Balance',
-                    value: '0 :star:s'
-                }, )
-                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-                .setTimestamp()
-                .setFooter('Grape Bank Inc.');
-            message.channel.send(balsolooEmbed);
-        } else {
-            const balsoloEmbed = new d.Discord.MessageEmbed()
-                .setColor('#dd2de0')
-                .setTitle(personName + `'s balance`)
-                .addFields({
-                    name: 'Balance',
-                    value:  `${bal}` + ' :star:s'
-                }, )
-                .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
-                .setTimestamp()
-                .setFooter('Grape Bank Inc.');
-            message.channel.send(balsoloEmbed);
         }
+        bal();
 
     }
-bal();
-	
-	}
 };
