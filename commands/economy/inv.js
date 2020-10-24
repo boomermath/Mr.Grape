96ab89ee9a74e63358ff018297cd202984c5a65c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-24 16:50:57 -0400

    Update inv.js

--- commands/economy/inv.js
+++ commands/economy/inv.js
@@ -32,6 +32,7 @@ module.exports = {
             if (Object.keys(inv).length === 0) {invEmbed.addFields({name: 'nothing but cobwebs and dust m8', value: '_'});}
 	    else {
             for (const key in inv) {
+		if (inv.key === 0) {delete inv.key; await d.items.set(message.author.id, inv);}
                 invEmbed.addFields({
                     name: key.charAt(0).toUpperCase() + key.slice(1) + "(s)",
                     value: `${inv[key]}`
