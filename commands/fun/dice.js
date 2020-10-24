4b82fc06fe5f9816068eba748f3a90f782cda305
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-24 14:56:42 -0400

    Create dice.js

---
+++ commands/fun/dice.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'dice',
+	description: 'roll a die',
+	cooldown: 5,
+	execute(message, args, d) {
+      let roll = Math.floor(Math.random() * 5) + 1;
+      const dice = new d.Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(message.author.username + `'s dice`)
+          .addFields (
+          {name: 'Roll:', value: roll}
+          )
+					.setTimestamp()
+					.setFooter('Grape Dice Club');
+     message.channel.send(dice);
+   }
+};
