408b6d596077b481e3bbf7cb3888652e69eea6a1
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 13:23:35 -0400

    Delete t.js

--- commands/economy/t.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 't',
-	description: 'test',
-	cooldown: 0,
-	execute(message, args, d) {
-      const users = d.items.opts.store.query('SELECT * FROM keyv')
-      message.channel.send(users);
-   }
-};
