df08f68e6f02332abbfc014b5212bb3f3ace63a8
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 18:39:40 -0400

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -6,7 +6,7 @@ module.exports = {
 	 let og = Math.floor(Math.random() * 25) + 25;
 	 let random;
 	let inv = await d.items.get(message.author.id)
-	if (inv.starmagnet !== undefined && inv.starmagnet > 0) {random = og * (1 + (0.02 * inv.starmagnet));}
+	if (inv.starmagnet !== undefined && inv.starmagnet > 0) {random = Math.round(og * (1 + (0.02 * inv.starmagnet)));}
 	else {random = og;}
         const dailystarEmbed = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
