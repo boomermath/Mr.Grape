5c502c07fdfe1fdc9f297e91823aac7a72876363
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 18:46:37 -0400

    Update dig.js

--- commands/economy/dig.js
+++ commands/economy/dig.js
@@ -4,13 +4,15 @@ module.exports = {
 	description: 'dig to earn stars',
 	aliases: ['mine'],
 	cooldown: 30,
-	execute(message, args, d) {
-    	async function mine() {
+	async execute(message, args, d) {
 	let shovelBreak = Math.floor(Math.random() * 15) + 1;
 	let inv = await d.items.get(message.author.id);
 	let earn;
 	if (inv.shovel !== undefined && inv.shovel > 0) {earn = Math.round(Math.random() * 15) + 1;}
 	else {earn = Math.round(Math.random() * 6) + 1;}
+	let finalEarn;
+ 	if (inv.starmagnet !== undefined && inv.starmagnet > 0) {finalEarn = Math.round(earn * (1 + (0.02 * inv.starmagnet)));}
+	else {finalEarn = earn}
         const mine = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(message.author.username + `'s mine`)
@@ -29,7 +31,6 @@ module.exports = {
 	}
         message.channel.send(mine);
         d.addMoni(message.author.id, earn);
-	}
-		mine();
+	
     }
 };
