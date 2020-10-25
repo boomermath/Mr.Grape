2be178e332c5c6cfecc543ffe1938e36c68898ac
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 19:21:41 -0400

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -3,16 +3,18 @@ module.exports = {
 	aliases: ['job'],
 	description: 'do honest work to get stars',
 	cooldown: 30,
-	execute(message, args, d) {
-	async function work() {
+	async execute(message, args, d) {
 	let inv = await d.items.get(message.author.id);
-	let earn = Math.round(Math.random() * 7) + 1;
+	let initialEarn = Math.round(Math.random() * 7) + 1;
         let chooseWork = Math.round(Math.random() * 2);
         const ifEarn = [1,2,2,2];
 	if (chooseWork === 0 && inv.hasOwnProperty('orangedetector')) {for (let i = 0; i < inv.orangedetector; i++) {ifEarn.push(1);}}
 	else if (chooseWork === 1 && inv.hasOwnProperty('mangodetector')) {for (let i = 0; i < inv.mangodetector; i++) {ifEarn.push(1);}}
 	else if (chooseWork === 2 && inv.hasOwnProperty('carrotdetector')) {for (let i = 0; i < inv.carrotdetector; i++) {ifEarn.push(1);}}
 	let earnJob = Math.floor(Math.random() * ifEarn.length);
+	let earn;
+	if (inv.starmagnet !== undefined && inv.starmagnet > 0) {earn = Math.round(initialEarn * (1 + (0.02 * inv.starmagnet)));}
+	else {earn = initialEarn;}
         const situation = [
             ['Help Mr.Grape find his orange!', 'will you help me find my orange?\nit fell in a bush full of bananas over there, but i could not find it.\nPlease go there and find my orange.', 'Yay, you found my orange! Here, take ' + earn + ' :star:s!', "That's not my orange, that's a banana! Try again later."],
             ['Help Mr.Grape catch his mango!', 'I am trying to catch a flying mango, but it keeps disappearing.\nSo will you catch it and bring it to me?', 'Yay, you found my mango! Here, take ' + earn + ' :star:s!', "You didn't catch my mango? Too bad, try again next time"],
@@ -23,7 +25,7 @@ module.exports = {
         background = situation[chooseWork][1];
         if (ifEarn[earnJob] === 1) {
             outcome = situation[chooseWork][2];
-            d.addMoni(message.author.id, parseInt(earn));
+            d.addMoni(message.author.id, earn);
         } else {
             outcome = situation[chooseWork][3];
         }
@@ -42,7 +44,7 @@ module.exports = {
             .setFooter('Grape Enterprises');
 
         message.channel.send(job);
-	}
-work();
+	
+
     }
 };
