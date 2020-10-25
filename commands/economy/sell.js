871114fc957dc262e03d1955128b639a0fc68e9b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 17:50:10 -0400

    Update sell.js

--- commands/economy/sell.js
+++ commands/economy/sell.js
@@ -15,19 +15,23 @@ module.exports = {
           if (!Object.keys(d.itemShop).includes(item)) {message.channel.send("bruh wdym, that's not a valid item to sell"); return;}
           if (inv[item] === undefined) {message.channel.send("bruh you don't even have that item lol"); return;}
           if (numberOfItems > inv[item]) {message.channel.send("You don't have that many " + item + '(s)'); return;}
-          d.addMoni(message.author.id, (d.itemShop[item] / 2))
+          let gain = (d.itemShop[item] / 2) * numberOfItems;
+	  d.addMoni(message.author.id, gain);
           inv[item] -= numberOfItems;
 	  await d.items.set(message.author.id, inv);
           let sale;
-          if (numberOfItems === 1) {sale = "a " + d.itemShop[item]}
-          else {sale = numberOfItems + " " + d.itemShop[item] + "s"}
+          if (numberOfItems === 1) {sale = "a " + item}
+          else {sale = numberOfItems + " " + item + "s"}
           const sell = new d.Discord.MessageEmbed()
                 .setColor('#dd2de0')
                 .setTitle(message.author.username + "'s sale")
                 .addFields({
                     name: 'Transaction',
-                    value:'You successfully sold ' + sale + " for" + (d.itemShop[item] / 2) + " :star:s"
-                }, )
+                    value:'You successfully sold ' + sale + " for " + (d.itemShop[item] / 2) + " :star:s each!" 
+                }, {
+                    name: 'Profit',
+                    value: gain + " :star:s" 
+                },)
                 .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
                 .setTimestamp()
                 .setFooter('Grape Marketplaces');
