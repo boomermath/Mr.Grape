53db4ff1666cc0ba0866a1f49e3a0a36b6fec90e
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-25 19:00:07 -0400

    Update dig.js

--- commands/economy/dig.js
+++ commands/economy/dig.js
@@ -30,7 +30,7 @@ module.exports = {
 		await d.items.set(message.author.id, inv);
 	}
         message.channel.send(mine);
-        d.addMoni(message.author.id, earn);
+        d.addMoni(message.author.id, finalEarn);
 	
     }
 };
