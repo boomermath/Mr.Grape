a6e081c3dd56cbaeea82a86aad5075919bf303a7
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-28 11:11:37 -0400

    Update cat.js

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -13,6 +13,6 @@ module.exports = {
         let urlQuery = d.querystring.stringify(params);
         let submitURL = catapi + `/v1/images/search?${urlQuery}`;
         let pic = await d.r2.get(submitURL , {key} ).json;
-        message.channel.send(pic[0]); 
+        message.channel.send(pic[3]); 
     }
 };
