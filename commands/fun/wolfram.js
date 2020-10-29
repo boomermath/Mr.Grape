c6481b21e1cbb69e28b4484e216c3a24efd3f7f3
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 19:21:25 -0400

    Create wolfram.js

---
+++ commands/fun/wolfram.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'wolfram',
+    description: 'get info from wolfram alpha',
+    aliases: ['wolf']
+    cooldown: 3,
+    async execute(message, args, d) {
+        if (!args[0]) {return message.channel.send('whaddya want me to look up?');}
+        let key = process.env.WOLFRAM;
+        let wolfapi = `https://api.wolframalpha.com/v2/query?appid=${key}&input=${encodeURIComponent(args.join(' '))}&output=json&podindex=1`
+        let output = await d.r2.get(wolfapi).json;
+        message.channel.send(output); 
+    }
+};
