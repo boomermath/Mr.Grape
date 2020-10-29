b01f2f2ff4df57b0f6b87f30fc8527517a59a222
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-29 19:23:06 -0400

    Update wolfram.js

--- commands/fun/wolfram.js
+++ commands/fun/wolfram.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'wolfram',
     description: 'get info from wolfram alpha',
-    aliases: ['wolf']
+    aliases: ['wolf'],
     cooldown: 3,
     async execute(message, args, d) {
         if (!args[0]) {return message.channel.send('whaddya want me to look up?');}
