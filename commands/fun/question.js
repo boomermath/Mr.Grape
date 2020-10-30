9f8669c3b3ecf01132eb0641eb9de6c1600d3749
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-30 12:39:05 -0400

    Update question.js

--- commands/fun/question.js
+++ commands/fun/question.js
@@ -10,7 +10,7 @@ module.exports = {
         let answer = await d.r2(wolfapi).text;
         if (answer === 'No short answer available') {
         let simpleWolf = `https://api.wolframalpha.com/v1/simple?i=${encodeURIComponent(args.join(' '))}&background=black&foreground=white&layout=labelbar&appid=${key}`
-        let ans = await d.r2.get(simpleWolf).blob;
+        let ans = await d.r2(simpleWolf).blob;
         return message.channel.send(ans);
         }
         message.channel.send(answer);
