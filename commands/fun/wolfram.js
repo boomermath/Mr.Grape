f3d573f5564ef35a6d7eb9ee5196f6d5951af999
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-30 09:49:39 -0400

    Update wolfram.js

--- commands/fun/wolfram.js
+++ commands/fun/wolfram.js
@@ -6,7 +6,8 @@ module.exports = {
     async execute(message, args, d) {
         if (!args[0]) {return message.channel.send('whaddya want me to look up?');}
         let key = process.env.WOLFRAM;
-        let wolfapi = `https://api.wolframalpha.com/v1/result?i=${encodeURIComponent(args.join(' '))}`;
-        message.channel.send(d.r2.get(wolfapi, {key}).text);
+        let wolfapi = `https://api.wolframalpha.com/v1/result?i=${encodeURIComponent(args.join(' '))}&appid=${key}`;
+        let answer = await d.r2.get(wolfapi).response
+        message.channel.send();
     }
 };
