c9b011b5587abc69736ef5afac23027a37b0a2c8
boomermath <63861512+boomermath@users.noreply.github.com>
2020-10-30 11:35:03 -0400

    Update question.js

--- commands/fun/question.js
+++ commands/fun/question.js
@@ -6,7 +6,7 @@ module.exports = {
     async execute(message, args, d) {
         if (!args[0]) {return message.channel.send('whaddya want me to look up?');}
         let key = process.env.WOLFRAM;
-        let wolfapi = `https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(args.join(' '))}&format=plaintext&output=JSON&appid=${key}`;
+        let wolfapi = `https://api.wolframalpha.com/v1/result?i=${encodeURIComponent(args.join(' '))}&appid=${key}`;
         let answer = await d.r2(wolfapi).text;
         if (answer === 'No short answer available') {return message.channel.send('This shows that I can parse if it says no avail');}
         message.channel.send(answer);
