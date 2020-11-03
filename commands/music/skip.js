76671889ad9906d3901292d02f6ea43f9c7b444a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-02 19:18:51 -0500

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'queue',
 	description: 'play music from youtube',
-  aliases: ['q'],
+  	aliases: ['q'],
 	cooldown: 2,
 	async execute(message, args, d) {
     const channel= message.member.voice;
