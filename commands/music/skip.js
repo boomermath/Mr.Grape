159a36d7883bafa8eefc7fcc5cdd987885580884
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-02 19:18:34 -0500

    Create skip.js

---
+++ commands/music/skip.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'queue',
+	description: 'play music from youtube',
+  aliases: ['q'],
+	cooldown: 2,
+	async execute(message, args, d) {
+    const channel= message.member.voice;
+		if (!channel) return message.channel.send('Get in a voice channel if you wanna do stuff');
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send("There ain't any songs playin");
+		serverQueue.connection.dispatcher.end('Skipped dat song!');
+};
