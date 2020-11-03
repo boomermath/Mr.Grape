15de346215408ef3a2f6333aadfd8303f49a711a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-02 19:57:59 -0500

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -4,8 +4,8 @@ module.exports = {
 	cooldown: 2,
   	aliases: ['q'],
 	execute(message) {
-		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send("There isn't a song playin");
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue) return message.channel.send("There isn't a song playin");
     const q = Array.from(serverQueue.songs.map(song => `${song.title}`).join(',') + ',0');
     const queue = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
@@ -15,4 +15,5 @@ module.exports = {
             .setFooter('DJ Grape');
       for (var key in q) {queue.addFields({name: q[key], value: '_' })}
       message.channel.send(queue);
+	}
 };
