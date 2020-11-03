32f165776966be69925d7da7d2ba36863ac0fe5f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-02 20:01:22 -0500

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -4,9 +4,10 @@ module.exports = {
   	aliases: ['s'],
 	cooldown: 2,
 	async execute(message, args, d) {
-    const channel= message.member.voice;
+    		const channel = message.member.voice;
 		if (!channel) return message.channel.send('Get in a voice channel if you wanna do stuff');
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send("There ain't any songs playin");
 		serverQueue.connection.dispatcher.end('Skipped dat song!');
+	}
 };
