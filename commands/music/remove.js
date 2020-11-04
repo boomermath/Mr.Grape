f0bd40e4075dd9dbf4f574329e3a672927338e5f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-04 15:46:33 -0500

    Update remove.js

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -8,7 +8,7 @@ module.exports = {
    if (isNaN(parseInt(args[0])) || !args[0]) return message.channel.send('Give me a valid number so I can remove that song!');
    let remove = args[0] + 1;
    let arr = serverQueue.songs;
-   if (remove > arr.length || remove < 0) return message.channel.send('Bro that's not a valid song to remove.')
+   if (remove > arr.length || remove < 0) return message.channel.send('Bro that\'s not a valid song to remove.')
    message.channel.send('Removed ' + `**${arr[remove].title}**`)
    arr.splice(remove, 1);
   }
