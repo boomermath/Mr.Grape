1503a01ca775d60f8e85529c167a5a66f58821e2
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-04 20:15:45 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,10 +1,10 @@
 const { Util } = require('discord.js');
 const ytdl = require('ytdl-core');
-
+const YoutubeAPI = require('simple-youtube-api');
+const youtube = new YoutubeAPI(process.env.YOUTUBE);
 module.exports = {
 	name: 'play',
-	description: 'Play command.',
-	usage: '[command name]',
+	description: 'play music',
 	cooldown: 0,
 	async execute(message, args, d) {
 		const { channel } = message.member.voice;
@@ -12,18 +12,29 @@ module.exports = {
 		const permissions = channel.permissionsFor(message.client.user);
 		if (!permissions.has('CONNECT')) return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
 		if (!permissions.has('SPEAK')) return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
-
+		
+		const ytRegex = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
 		const serverQueue = message.client.queue.get(message.guild.id);
-		const songInfo = await ytdl.getInfo(args[0].replace(/<(.+)>/g, '$1'));
+		const argument = args.join(' ');
+		let songInfo;
+		if (ytRegex.test(argument)) {
+		songInfo = await youtube.getVideo(argument);
+		songInfo.url = argument;
+		}
+		else {
+		let video = await youtube.searchVideos(argument, 1);
+		songInfo.url = video[0].url;
+		songInfo = await youtube.getVideo(songInfo.url);
+		}
 		const song = {
-			id: songInfo.videoDetails.video_id,
-			title: Util.escapeMarkdown(songInfo.videoDetails.title),
-			url: songInfo.videoDetails.video_url
+			title: songInfo.title,
+			url: songInfo.url,
+			duration: songInfo.duration, 
+			thumbnail: songInfo.thumbnails.high.url
 		};
 
 		if (serverQueue) {
 			serverQueue.songs.push(song);
-			console.log(serverQueue.songs);
 			return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
 		}
 
@@ -46,7 +57,7 @@ module.exports = {
 				return;
 			}
 
-			const dispatcher = queue.connection.play(ytdl(song.url))
+			const dispatcher = queue.connection.play(ytdl(song.url, {filter: audioonly}))
 				.on('finish', () => {
 					queue.songs.shift();
 					play(queue.songs[0]);
