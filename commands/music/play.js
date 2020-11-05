1c17e1c2638052d017b72ec54c65d9036da0b65c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-04 21:01:43 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -29,7 +29,7 @@ module.exports = {
 		const song = {
 			title: songInfo.title,
 			url: songInfo.url,
-			duration: songInfo.duration, 
+			duration: 0, 
 			thumbnail: songInfo.thumbnails.high.url
 		};
 
