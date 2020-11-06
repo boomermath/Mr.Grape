6a1a42345c34def7a538e6190a5d4dfc9afe884b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-06 18:07:04 -0500

    Update np.js

--- commands/music/np.js
+++ commands/music/np.js
@@ -1,15 +1,23 @@
 module.exports = {
 	name: 'nowplaying',
-	description: 'get see what song is currently playin',
+	description: 'see what song is currently playin',
 	cooldown: 2,
   	aliases: ['np'],
 	execute(message, args, d) {
+	function convertTime(youtubeFormattedTime) {
+		let vidTime = qtime.split(':');
+		let final;
+		if (vidTime.length === 2 && vidTime[0] !== 0) {final = (vidTime[0] * 60) + (vidTime[1])}
+		else if (vidTime.length === 3) {final = (vidTime[0] * 3600) + (vidTime[1] * 60) + (vidTime[2]);}
+		else {final = vidTime[1]}
+		return final;
+	}
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue) return message.channel.send("Bruh wdym there is nothing playin");
     const q = serverQueue.songs[0];
     const seek = (serverQueue.connection.dispatcher.streamTime - serverQueue.connection.dispatcher.pausedTime) / 1000;
-    const timePlaying = q - seek;
-    const show = new Date(timePlaying * 1000).toISOString().substr(11, 8);
+    const timeLeft = convertTime(q.duration) - seek;
+    const show = new Date(timeLeft * 1000).toISOString().substr(11, 8);
     const np = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle('Now Playing')
@@ -17,7 +25,10 @@ module.exports = {
     	    .setThumbnail(q.thumbnail)
             .addFields(
             {name: `${q.title}`, value: '\u200b'},
-            {name: `${show}/${q.duration}` , value: '_'},
+            {name: 'Time elapsed' , value: `${new Date(seek * 1000).toISOString().substr(11, 8)}`},
+            {name: 'Time remaining' , value: timeLeft},
+            {name: 'Total Duration' , value: `${q.duration}`},
+
             )
             .setTimestamp()
             .setFooter('DJ Grape');
