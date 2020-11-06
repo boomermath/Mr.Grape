d28c442771dc2dc8b76b8860c30778a5db82d651
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-06 14:05:52 -0500

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -8,10 +8,13 @@ module.exports = {
         if (!serverQueue) return message.channel.send('There\'s nothin playin!');
         if (serverQueue.repeatMode === 0) {
             serverQueue.repeatMode = 1;
-            message.channel.send('Now looping through the **queue**')
+            message.channel.send('Now looping through the **song**')
         } else if (serverQueue.repeatMode === 1) {
+            serverQueue.repeatMode = 2;
+            message.channel.send('Now looping through the **queue**')
+        } else if (serverQueue.repeatMode === 2) {
             serverQueue.repeatMode = 0;
-            message.channel.send('Looping turned off.')
+            message.channel.send('Looping is turned off.')
         }
     }
 };
