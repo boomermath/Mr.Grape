63bc3024185eae87f559f5b951d5bf0fb7900898
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-06 18:47:51 -0500

    Update np.js

--- commands/music/np.js
+++ commands/music/np.js
@@ -4,22 +4,13 @@ module.exports = {
 	cooldown: 2,
   	aliases: ['np'],
 	execute(message, args, d) {
-	function convertTime(youtubeFormattedTime) {
-		let vidTime = youtubeFormattedTime.split(':');
-		let final;
-		if (vidTime.length === 2 && vidTime[0] !== 0) {final = (vidTime[0] * 60) + (vidTime[1])}
-		else if (vidTime.length === 3) {final = (vidTime[0] * 3600) + (vidTime[1] * 60) + (vidTime[2]);}
-		else {final = vidTime[1]}
-		return final;
-	}
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue) return message.channel.send("Bruh wdym there is nothing playin");
     const q = serverQueue.songs[0];
+    const duration = q.duration.split(':').reverse().reduce((prev, curr, i) => prev + curr*Math.pow(60, i), 0)
     const actualSeek = ((serverQueue.connection.dispatcher.streamTime - serverQueue.connection.dispatcher.pausedTime) / 1000)
     const seek = new Date(actualSeek * 1000).toISOString().substr(11,8);
-    const timeLeft = new Date((convertTime(q.duration) - actualSeek) * 1000).toISOString().substr(11,8);
-    if (seek.startsWith('00:00:')) {seek.replace('00:')}
-    if (timeLeft.startsWith('00:00:')) {timeLeft.replace('00:')}
+    const timeLeft = new Date((duration - actualSeek) * 1000).toISOString().substr(11,8);
     const np = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle('Now Playing')
