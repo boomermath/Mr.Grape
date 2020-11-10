f1db8d4dc5bc8c621f8ff4392289d894aa867cb8
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-10 09:41:15 -0500

    Update dig.js

--- commands/economy/dig.js
+++ commands/economy/dig.js
@@ -39,21 +39,21 @@ module.exports = {
            mine.addField(`You got ${amount} ${ore}(s)!`,'_')
         }
         
-        if (inv.t3p) {
+        if (inv.tierthreepick) {
            if (randNum(2) === 1) {pick(randArray(ores.tier1), randNum(7));}
            if (randNum(3) === 1) {pick(randArray(ores.tier2), randNum(4));}
            if (randNum(7) === 1) {pick(randArray(ores.tier3), randNum(3));}
            if (randNum(100) === 1) {mine.addField('Uh oh!', 'Your pickaxe broke, buy a new one from the shop!'); inv.tierthreepick -= 1;}
           await d.items.set(message.author.id, inv);
         }
-        else if (inv.t2p) {
+        else if (inv.tiertwopick) {
            if (randNum(3) === 1) {pick(randArray(ores.tier1), randNum(5));}
            if (randNum(5) === 1) {pick(randArray(ores.tier2), randNum(3));}
            if (randNum(35) === 1) {pick(randArray(ores.tier3), 1);}
            if (randNum(50) === 1) {mine.addField('Uh oh!', 'Your pickaxe broke, buy a new one from the shop!'); inv.tiertwopick -= 1;}
           await d.items.set(message.author.id, inv);
         }
-        else if (inv.t1p) {
+        else if (inv.tieronepick) {
            if (randNum(4) === 1) {pick(randArray(ores.tier1), randNum(3));}
            if (randNum(25) === 1) {pick(randArray(ores.tier2), 1);}
            if (randNum(15) === 1) {mine.addField('Uh oh!', 'Your pickaxe broke, buy a new one from the shop!'); inv.tieronepick -= 1;}
