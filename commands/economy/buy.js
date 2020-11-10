e8975127323a48b2e285889a15369ed43a7df683
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-10 09:27:11 -0500

    Update buy.js

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -9,7 +9,6 @@ module.exports = {
         let numberOfItemsRaw = args.join(' ').match(regex);
         let numberOfItems = parseInt(numberOfItemsRaw);
         let item = args.join(' ').replace(numberOfItems, '').replace(' ', '');
-                message.channel.send(item);
         if (have === undefined || have === null) {
             have = {};
         }
