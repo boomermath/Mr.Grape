61665aa28893d5f156c5d576afc8b3250ee16bc9
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-11 11:44:15 -0500

    Update ore.js

--- commands/economy/ore.js
+++ commands/economy/ore.js
@@ -9,7 +9,7 @@ module.exports = {
                 .toLowerCase()
                 .split('\n')
                 .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                .join('\n');
+                .join('\n ');
         };
 
         const ore = new d.Discord.MessageEmbed()
