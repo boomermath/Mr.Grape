d4019546151e9dd722eada53147ca1205927fa65
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-11 20:27:38 -0500

    Update randomevent.js

--- utils/randomevent.js
+++ utils/randomevent.js
@@ -1,8 +1,2 @@
 const Discord = require('discord.js')
-module.exports = {
-    run: {
-       if (message.content.includes('llama')) {
-           message.channel.send('baaaaaaaaaaaa');
-       }
-    }
-};
+message.channel.send('ayy it works');
