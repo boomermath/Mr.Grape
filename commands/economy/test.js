1aa910e2d0e60f64ded6c28baa0a61baa0acec30
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-16 14:48:47 -0500

    Update test.js

--- commands/economy/test.js
+++ commands/economy/test.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js')
+const client = new Discord.Client();
 module.exports = {
     name: 'test',
     description: 'test',
     cooldown: 0,
     execute(message, args, d) {
-        const emoji = message.client.emojis.find(emoji => emoji.name === "silver");
+        const emoji = client.emojis.find(emoji => emoji.name === "silver");
         message.reply(`${emoji} silver look cool`);
     }
 };
