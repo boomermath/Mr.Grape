eb82fcc9ce610d2c992501b1cbe7829e237cf66a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-16 14:44:45 -0500

    Update test.js

--- commands/economy/test.js
+++ commands/economy/test.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'test',
     cooldown: 0,
     execute(message, args, d) {
-        const emoji = message.client.emojis.get('777977486172618812');
+        const emoji = message.client.emojis.get("777977486172618812");
         message.reply(`${emoji} silver look cool`);
     }
 };
