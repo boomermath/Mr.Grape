1af85ca82b31a99a81b1508aee81ae0ace263b21
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-17 10:55:11 -0500

    Update test.js

--- commands/economy/test.js
+++ commands/economy/test.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
-const client = new Discord.Client();
 module.exports = {
     name: 'test',
     description: 'test',
     cooldown: 0,
     execute(message, args, d) {
         const someEmoji = message.client.emojis.cache.get("778264742606798860");
+        console.log(someEmoji);
         message.channel.send(`${someEmoji}`);
     }
 };
