9756940a8cb326701007e708e5d51a355ba5e295
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-17 11:02:14 -0500

    Update test.js

--- commands/economy/test.js
+++ commands/economy/test.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'test',
     cooldown: 0,
     execute(message, args, d) {
-        const someEmoji = message.client.emojis.cache.get("778264742606798860");
+        const someEmoji = message.client.emojis.cache.get("776578867988267059");
         console.log(message.client.emojis.cache);
         message.channel.send(`${someEmoji}`);
     }
