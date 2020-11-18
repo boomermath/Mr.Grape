25b08db46cc7e3d217cad560a2a88019babefbba
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-18 11:16:51 -0500

    Update lb.js

--- commands/economy/lb.js
+++ commands/economy/lb.js
@@ -8,7 +8,7 @@ module.exports = {
         for (const member in members) {
             let m = members[member];
             const userBal = await d.users.get(m)
-            if (!userBal || userBal <= 0) { null; }
+            if (!userBal || userBal === 0) { null; }
             else { lbObj[m] = userBal }
         }
         const sort = Object.entries(lbObj).sort((a, b) => b[1] - a[1]);
