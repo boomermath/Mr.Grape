5a1f585f6536a7deb3bb73423705ba8ca4d9bd87
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-18 12:06:03 -0500

    Update lb.js

--- commands/economy/lb.js
+++ commands/economy/lb.js
@@ -4,9 +4,12 @@ module.exports = {
     async execute(message, args, d) {
         const lbObj = {};
         message.guild.members.cache.forEach(member => {
-            let userBal = await d.users.get(member.id);
-            if (!userBal || userBal <= 0) { null; }
-            else { lbObj[member.displayName] = userBal }
+            async function lb(member) {
+                let userBal = await d.users.get(member.id);
+                if (!userBal || userBal <= 0) { null; }
+                else { lbObj[member.displayName] = userBal }
+            }
+            lb(member);
         });
         const sort = Object.entries(lbObj).sort((a, b) => b[1] - a[1]);
         message.channel.send(sort);
