0d7433fc371c92b7b8a5e5ad7256d583c4bfa1f3
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-18 08:57:28 -0500

    Update lb.js

--- commands/economy/lb.js
+++ commands/economy/lb.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args, d) {
         const guild = message.guild.id;
         const lbObj = {};
-        guild.members.cache.each(member => {
+        guild.members.fetch().forEach(member => {
             async function lb() {
                 let userBal = await d.users.get(member.id);
                 if (userBal === 0 || !userBal) { null; }
