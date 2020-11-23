b770ad62ad76fef4cb56eea4ef5a3eee36a16169
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-23 11:37:09 -0500

    Update apod.js

--- commands/fun/apod.js
+++ commands/fun/apod.js
@@ -5,7 +5,7 @@ module.exports = {
     cooldown: 3,
     async execute(message, args, d) {
         let NASAURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA}`
-        let pic = await d.r2.get(NASAUR).json;
+        let pic = await d.r2.get(NASAURL).json;
         message.channel.send({ files: [pic.hdurl] });
     }
 };
