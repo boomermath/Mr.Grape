9fd142b9ce4c8692f90170def2aeb1cf7e413c81
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-24 14:39:55 -0500

    move recipes to a new file

--- commands/economy/craft.js
+++ commands/economy/craft.js
@@ -1,27 +1,21 @@
+const recipe = require('../utils/recipes');
 module.exports = {
     name: "craft",
     cooldown: 0,
     async execute(message, args, d) {
+        let argument = args.join(' ');
         let inv = await d.items.get(message.author.id);
-        const fakeRecipe = {
-            "bronze": 1,
-            "iron": 1,
-            createditem: 'rigged dice'
-        }
-        const recipeCollection = {
-            "test": fakeRecipe
-        }
-        if (Object.keys(recipeCollection).includes(args[0])) {
-            let recipe = Object.keys(recipeCollection).find(v => args[0].includes(v));
-            for (const key in recipeCollection[recipe]) {
+        if (Object.keys(recipe).some(e => argument.includes(e))) {
+            let craft = Object.keys(recipe).find(e => argument.includes(e));
+            for (const key in recipe[craft]) {
                 if (key === 'createditem') { continue; }
-                if (!inv.ore[key] || inv.ore[key] < recipeCollection[recipe]) {
-                    return message.channel.send('you dont have the sutff');
+                if (!inv.ore[key] || inv.ore[key] < recipe[craft][key]) {
+                    return message.channel.send('you dont have the stuff');
                 }
-                inv.ore[key] -= recipeCollection[recipe][key];
+                inv.ore[key] -= recipe[craft][key];
             }
-            if (!inv[recipeCollection[recipe].createditem]) { inv[recipeCollection[recipe].createditem] = 1 }
-            else { inv[recipeCollection[recipe].createditem] += 1 }
+            if (!inv[recipe[craft].createditem]) { inv[recipe[craft].createditem] = 1 }
+            else { inv[recipe[craft].createditem] += 1 }
             await d.items.set(message.author.id, inv);
         }
         else { message.channel.send('idk bro') }
