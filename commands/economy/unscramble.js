389736f65e4719c5f4382dae58b6b0932bcd891c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-24 11:03:59 -0500

    a

--- commands/economy/unscramble.js
+++ commands/economy/unscramble.js
@@ -1,8 +1,9 @@
 module.exports = {
-    name: "task",
+    name: "unscramble",
+    aliases: ['unsc'],
     description: "unscramble a word for $$$",
     cooldown: 0,
-    execute(message, args, d) {
+    async execute(message, args, d) {
         const fruitArray = [
             'apple', 'apricot', 'avocado', 'banana', 'berry', 'blackberry', 'orange', 'blueberry', 'boysenberry', 'breadfruit', 'cantaloupe', 'cherry', 'citron', 'citrus', 'coconut', 'crabapple', 'cranberry', 'currant', 'date', 'durian', 'elderberry', 'fig', 'grape', 'grapefruit', 'guava', 'honeydew', 'jackfruit', 'kiwi', 'kumquat', 'lemon', 'lime', 'lingonberry', 'loquat', 'lychee', 'mandarin', 'mango', 'melon', 'mulberry', 'nectarine', 'papaya', 'peach', 'pear', 'persimmon', 'pineapple', 'plantain', 'plum', 'pluot', 'pomegranate', 'pomelo', 'prune', 'quince', 'raisin', 'raspberry', 'strawberry', 'tangelo', 'tangerine', 'watermelon'
         ]
@@ -18,7 +19,7 @@ module.exports = {
         const scrambleEmbed = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(message.author.username + `'s task`)
-            .addField('Unscramble', `${scrambledWord}`)
+            .addField('Unscramble the fruit!', `${scrambledWord}`)
             .setTimestamp()
             .setFooter('Grape Enterprises');
         message.channel.send(scrambleEmbed);
@@ -31,7 +32,13 @@ module.exports = {
             .then(async message => {
                 message = message.first()
                 if (message.content.toLowerCase() === word) {
-                    message.channel.send(`Yay! You guessed the word correctly! You get some :star:s`)
+                    let e = Math.floor(Math.random() * 7) + 1;
+                    let inv = await d.items.get(message.author.id);
+                    if (inv.starmagnet) {
+                        e = Math.round(e * (1 + (0.02 * inv.starmagnet)));
+                    }
+                    d.addMoni(e)
+                    message.channel.send(`Yay! You guessed the word correctly! You get ${e} :star:s`)
                 } else {
                     message.channel.send(`bruh ur bad, the word was ${word} duh`)
                 }
