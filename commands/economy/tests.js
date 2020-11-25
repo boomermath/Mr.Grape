96458555a78536089ae3f869c8a7c171e8e9690f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-25 13:40:15 -0500

    Update tests.js

--- commands/economy/tests.js
+++ commands/economy/tests.js
@@ -4,12 +4,12 @@ module.exports = {
       execute(message, args, d) {
             const mine = new d.Discord.MessageEmbed()
                   .setColor('#dd2de0')
-                  .setTitle(message.author.username + `'s mine`)
+                  .setTitle('test')
                   .addField('field 1', '_')
                   .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
                   .setTimestamp()
                   .setFooter('Grape Mining Guild');
 
-            message.edit(mine.addField('field 2', '_'))
+            mine.edit(mine.addField('field 2', '_'))
       }
 };
