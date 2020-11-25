3ec97b2ae03c1acd24775fc58c51daa7435121c1
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-25 09:56:11 -0500

    I have no idea

--- commands/economy/recipe.js
+++ commands/economy/recipe.js
@@ -23,7 +23,7 @@ module.exports = {
             .setFooter('Grape Maker Thingy');
         for (const key in recipe[item]) {
             if (key === 'createditem') { continue; }
-            let orePic = d.emoji[d.ores.tier1.concat(d.ores.tier2, d.ores.tier3).find(v => item.replace("refined ").includes(v))];
+            let orePic = d.emoji[d.ores.tier1.concat(d.ores.tier2, d.ores.tier3).find(v => key.replace("refined ").includes(v))];
             recipeItem.addField(orePic + " - " + key.charAt(0).toUpperCase() + key.slice(1) + "(s) ", recipe[item][key])
         }
         message.channel.send(recipeItem);
