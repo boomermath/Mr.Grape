33020842f9b0a5f431533b773b0be21f19afb1e8
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-26 16:58:16 -0500

    Update sell.js

--- commands/economy/sell.js
+++ commands/economy/sell.js
@@ -95,7 +95,7 @@ module.exports = {
 
                 message.channel.send(sale);
             }
-            if (!inv.starmill || inv.starmill === 0 && inv.time.starmill) { delete inv.time.starmill; }
+            if (!inv.starmill || inv.starmill === 0 && inv.time && inv.time.starmill) { delete inv.time.starmill; }
             await d.items.set(message.author.id, inv);
         }
         else if (oreConcat.some(e => argument.includes(e)) || argument.includes('ores' || 'ore')) {
