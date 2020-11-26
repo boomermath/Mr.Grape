5baa30a3668c21d5999ed9ca917cc138d397c56b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-26 11:14:41 -0500

    Update tests.js

--- commands/economy/tests.js
+++ commands/economy/tests.js
@@ -1,25 +1,37 @@
+const gamble = require("./gamble");
+
 module.exports = {
       name: "test",
       cooldown: 0,
-      execute(message, args, d) {
-            const gambleEmbed = new d.Discord.MessageEmbed()
-                  .setColor('#dd2de0')
-                  .setTitle(message.author.username + `'s gambling table`)
-                  .addFields({
-                        name: '--------------',
-                        value: 'ok, if you roll an even number you win, if you roll an odd number, you lose'
-                  })
-                  .setTimestamp()
-                  .setFooter('Grape Gambling Club.');
-            message.channel.send(gambleEmbed)
-                  .then((msg) => {
-                        setTimeout(function () {
-                              msg.edit(gambleEmbed.addField('--------------', 'you rolled a . . .')).then((msg) => {
-                                    setTimeout(function () {
-                                          msg.edit(gambleEmbed.addField('--------------', '<insert number here>'));
-                                    }, 2000)
-                              });
-                        }, 2000)
-                  })
+      async execute(message, args, d) {
+            function animateEmbed(diceRoll, bet) {
+                  const gambleEmbed = new d.Discord.MessageEmbed()
+                        .setColor('#dd2de0')
+                        .setTitle(message.author.username + `'s gambling table`)
+                        .addFields('Ok, if you roll an even number you win, if you roll an odd number, you lose.', '_')
+                        .setTimestamp()
+                        .setFooter('Grape Gambling Club.');
+                  message.channel.send(gambleEmbed)
+                        .then((msg) => {
+                              setTimeout(function () {
+                                    msg.edit(gambleEmbed.addField('You rolled a . . .', '_')).then((msg) => {
+                                          setTimeout(function () {
+                                                msg.edit(gambleEmbed.addField(diceRoll, '_')).then((msg) => {
+                                                      setTimeout(function () {
+                                                            if (diceRoll % 2 === 0) {
+                                                                  msg.edit(gambleEmbed.addField(`Congrats, you get ${bet} :star:s!`, '_'));
+                                                                  return true;
+                                                            }
+                                                            else {
+                                                                  msg.edit(gambleEmbed.addField('Rip, you lost your :star:s.', '_'));
+                                                                  return false;
+                                                            }
+                                                      }, 2000)
+                                                });
+                                          }, 4000)
+                                    });
+                              }, 2000)
+                        })
+            }
       },
 };
