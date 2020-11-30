8dd70835f3e9f87334f997b1f2ec052cb986eeda
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-30 14:08:29 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -46,7 +46,7 @@ module.exports = {
 		}
 		if (ytRegex.test(argument) && plRegex.test(argument)) {
 			const playlist = await youtube.getPlaylist(argument);
-			console.log(playlist.id)
+			console.log(playlist.videos)
 			/*
 			for (video in playlist.videos) {
 				let plSong = playlist.videos[video];
