67252139248323222131c8a43cfaf55d42a9418f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-11-30 09:25:34 -0500

    okok i have an idea

--- commands/music/play.js
+++ commands/music/play.js
@@ -19,100 +19,98 @@ module.exports = {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		const argument = args.join(' ');
 		let songInfo;
+		function createSong(title, url, duration, thumbnail) {
+			const song = {
+				"title": title,
+				"url": url,
+				"duration": duration,
+				"thumbnail": thumbnail
+			}
+			return song;
+		}
 		if (ytRegex.test(argument) && plRegex.test(argument)) {
 			const playlist = await youtube.getPlaylist('https://www.youtube.com/playlist?list=PLAuXvMFaTiZwojnLr7JLOupJCikzwShYH');
 			console.log(playlist)
 			for (video in playlist.videos) {
 				let plSong = playlist.videos[video];
-				var tempArray = [];
-				const song = {
-					title: Util.escapeMarkdown(plSong.title),
-					url: `https://www.youtube.com/watch?v=${plSong.id}`,
-					duration: plSong.durationFormatted,
-					thumbnail: plSong.thumbnail.url
-				};
-				tempArray.push(song);
+				let song = createSong(Util.escapeMarkdown(plSong.title), `https://www.youtube.com/watch?v=${plSong.id}`, plSong.durationFormatted, plSong.thumbnail.url)
+				playSong(song, message, channel, serverQueue)
 			}
-			console.log(tempArray)
-			if (serverQueue) { return serverQueue.songs.push(...tempArray); }
 		}
 		else {
 			songInfo = await youtube.searchOne(argument);
-			var song = {
-				title: Util.escapeMarkdown(songInfo.title),
-				url: songInfo.url,
-				duration: songInfo.durationFormatted,
-				thumbnail: songInfo.thumbnail.url
+			let song = createSong(Util.escapeMarkdown(songInfo.title), songInfo.url, songInfo.durationFormatted, songInfo.thumbnail.url)
+			playSong(song, message, channel, serverQueue)
+		}
+
+		async function playSong(video, message, vc, queue) {
+			if (queue) {
+				queue.songs.push(song);
+				const added = new d.Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(song.title)
+					.setURL(song.url)
+					.setDescription(`Duration: ${song.duration}`)
+					.setThumbnail(song.thumbnail)
+					.addField('Added to the queue!', '_')
+					.setTimestamp()
+					.setFooter('DJ Grape');
+				return message.channel.send(added);
+			}
+
+			const queueConstruct = {
+				textChannel: message.channel,
+				voiceChannel: channel,
+				connection: null,
+				songs: [],
+				volume: 2,
+				playing: true,
+				repeatMode: 0,
 			};
-		}
 
+			message.client.queue.set(message.guild.id, queueConstruct);
+			queueConstruct.songs.push(song);
 
-		if (serverQueue) {
-			serverQueue.songs.push(song);
-			const added = new d.Discord.MessageEmbed()
-				.setColor('#dd2de0')
-				.setTitle(song.title)
-				.setURL(song.url)
-				.setDescription(`Duration: ${song.duration}`)
-				.setThumbnail(song.thumbnail)
-				.addField('Added to the queue!', '_')
-				.setTimestamp()
-				.setFooter('DJ Grape');
-			return message.channel.send(added);
-		}
+			const play = async song => {
+				const queue = message.client.queue.get(message.guild.id);
+				if (!song) { return; }
 
-		const queueConstruct = {
-			textChannel: message.channel,
-			voiceChannel: channel,
-			connection: null,
-			songs: [],
-			volume: 2,
-			playing: true,
-			repeatMode: 0,
-		};
+				let stream = ytdl(song.url, {
+					filter: "audioonly",
+					quality: "highestaudio"
+				});
 
-		message.client.queue.set(message.guild.id, queueConstruct);
-		queueConstruct.songs.push(song);
+				const dispatcher = queue.connection.play(stream)
+					.on('finish', () => {
+						if (queue.repeatMode === 0) { queue.songs.shift(); }
+						else if (queue.repeatMode === 2) { queue.songs.push(queue.songs.shift()); }
+						else { null; }
+						play(queue.songs[0]);
+					})
+					.on('error', error => console.error(error));
+				dispatcher.setVolumeLogarithmic(queue.volume / 5);
+				const started = new d.Discord.MessageEmbed()
+					.setColor('#dd2de0')
+					.setTitle(song.title)
+					.setURL(song.url)
+					.setDescription(`Duration: ${song.duration}`)
+					.setThumbnail(song.thumbnail)
+					.addField('Groovin to the tunes!', '_')
+					.setTimestamp()
+					.setFooter('DJ Grape');
+				queue.textChannel.send(started);
+			};
 
-		const play = async song => {
-			const queue = message.client.queue.get(message.guild.id);
-			if (!song) { return; }
-
-			let stream = ytdl(song.url, {
-				filter: "audioonly",
-				quality: "highestaudio"
-			});
-
-			const dispatcher = queue.connection.play(stream)
-				.on('finish', () => {
-					if (queue.repeatMode === 0) { queue.songs.shift(); }
-					else if (queue.repeatMode === 2) { queue.songs.push(queue.songs.shift()); }
-					else { null; }
-					play(queue.songs[0]);
-				})
-				.on('error', error => console.error(error));
-			dispatcher.setVolumeLogarithmic(queue.volume / 5);
-			const started = new d.Discord.MessageEmbed()
-				.setColor('#dd2de0')
-				.setTitle(song.title)
-				.setURL(song.url)
-				.setDescription(`Duration: ${song.duration}`)
-				.setThumbnail(song.thumbnail)
-				.addField('Groovin to the tunes!', '_')
-				.setTimestamp()
-				.setFooter('DJ Grape');
-			queue.textChannel.send(started);
-		};
-
-		try {
-			const connection = await channel.join();
-			queueConstruct.connection = connection;
-			play(queueConstruct.songs[0]);
-		} catch (error) {
-			console.error(`I could not join the voice channel: ${error}`);
-			message.client.queue.delete(message.guild.id);
-			await channel.leave();
-			return message.channel.send(`I could not join the voice channel: ${error}`);
+			try {
+				const connection = await channel.join();
+				queueConstruct.connection = connection;
+				play(queueConstruct.songs[0]);
+			} catch (error) {
+				console.error(`I could not join the voice channel: ${error}`);
+				message.client.queue.delete(message.guild.id);
+				await channel.leave();
+				return message.channel.send(`I could not join the voice channel: ${error}`);
+			}
 		}
 	}
 };
