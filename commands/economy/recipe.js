bffc5f0cbefed5c9e37f1c8120c95561d53af08a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-01 17:52:53 -0500

    Update recipe.js

--- commands/economy/recipe.js
+++ commands/economy/recipe.js
@@ -18,11 +18,11 @@ module.exports = {
         const recipeItem = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(`Recipe for ${item.charAt(0).toUpperCase() + item.slice(1)}`)
-            .setDescription(item['description'])
+            .setDescription(item.description)
             .setTimestamp()
             .setFooter('Grape Maker Thingy');
         for (const key in recipe[item]) {
-            if (key === 'createditem') { continue; }
+            if (key === 'createditem' || key === 'description') { continue; }
             let orePic = d.emoji[d.ores.tier1.concat(d.ores.tier2, d.ores.tier3).find(v => key.replace("refined ").includes(v))];
             recipeItem.addField(orePic + " - " + key.charAt(0).toUpperCase() + key.slice(1) + "(s) ", recipe[item][key])
         }
