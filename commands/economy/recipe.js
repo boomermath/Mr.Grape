02bd44e2420e19cb4196b5cb8526ecb1e23eb66b
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-01 17:50:06 -0500

    a

--- commands/economy/recipe.js
+++ commands/economy/recipe.js
@@ -17,8 +17,8 @@ module.exports = {
         let item = Object.keys(recipe).find(e => argument.includes(e));
         const recipeItem = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
-            .setTitle('Recipe')
-            .setDescription(item.charAt(0).toUpperCase() + item.slice(1))
+            .setTitle(`Recipe for ${item.charAt(0).toUpperCase() + item.slice(1)}`)
+            .setDescription(item['description'])
             .setTimestamp()
             .setFooter('Grape Maker Thingy');
         for (const key in recipe[item]) {
