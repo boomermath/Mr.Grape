34dfa0d57f61e0de749ef3e0ebfc06d6f921082f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-01 10:25:26 -0500

    Create connection.js

---
+++ commands/music/connection.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'connection',
+	description: 'pause a song that\'s playing',
+	cooldown: 2,
+	execute(message, args, d) {
+    const q = message.client.queue.get(message.guild.id);
+    message.channel.send(q.dispatcher);	
+	}
+};
