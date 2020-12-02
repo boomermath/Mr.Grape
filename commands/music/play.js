8fbf114b484cafe26f6a4c4e9b89d5b73d862fb1
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-02 11:33:11 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -20,7 +20,10 @@ module.exports = {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		const argument = args.join(' ');
 
-		if (serverQueue && serverQueue.songs && !message.guild.voiceConnection) { message.client.queue.delete(message.guild.id); }
+		if (serverQueue && !message.guild.voiceConnection) { 
+			serverQueue.voiceChannel.leave();
+			message.client.queue.delete(message.guild.id); 
+		}
 		
 		function createSong(title, url, duration, thumbnail) {
 			const song = {
