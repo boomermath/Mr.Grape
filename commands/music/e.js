1f9a80b9b9ba3c0bb14bbcdde681264f5b367d13
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-03 09:48:30 -0500

    Update e.js

--- commands/music/e.js
+++ commands/music/e.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'stop playing music',
 	cooldown: 2,
 	execute(message, args, d) {
-        const { currentChannel } = message.client.voice.channel;
+        const { currentChannel } = message.client.user.voice.channel;
         message.channel.send(currentChannel)
 	}
 };
