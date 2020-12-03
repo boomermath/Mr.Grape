7123478230236c41599493cdbf2ec978c568d915
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-03 09:14:00 -0500

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -4,11 +4,13 @@ module.exports = {
 	cooldown: 2,
 	aliases: ['leave', 'disconnect', 'dc'],
 	execute(message, args, d) {
-		const channel = message.member.voice;
-		if (!channel) return message.channel.send("Go to a voice channel to do stuff");
-		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send("There ain't any music!");
-		serverQueue.voiceChannel.leave();
-		message.client.queue.delete(message.guild.id);
+		try {
+			const channel = message.member.voice;
+			if (!channel) return message.channel.send("Go to a voice channel to do stuff");
+			const serverQueue = message.client.queue.get(message.guild.id);
+			serverQueue.voiceChannel.leave();
+			message.client.queue.delete(message.guild.id);
+		}
+		catch { message.channel.send('There\'s no music!') }
 	}
 };
