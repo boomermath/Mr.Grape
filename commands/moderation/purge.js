93a9c052c16d936b9bbc93017710eacf7738f06c
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-03 10:14:54 -0500

    a

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,10 +1,17 @@
 module.exports = {
-	name: 'purge',
-	description: 'purge messages from a channel',
-	cooldown: 0,
-	execute(message, args, d) {
+    name: 'purge',
+    description: 'purge messages from a channel',
+    cooldown: 0,
+    execute(message, args, d) {
         if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You don\'t have perms!')
         if (!args[0]) return message.channel.send('Bruh how many messages should I purge?');
-        message.channel.bulkDelete(parseInt(args[0]));
-	}
+        let number = parseInt(args[0]);
+        let iteration = ~~(number / 100);
+        let leftover = number - (iteration * 100);
+        for (let i = 0; i < iteration; i++) {
+            message.channel.bulkDelete(100);
+        }
+        if (leftover > 0) { message.channel.bulkDelete(leftover); }
+        message.channel.send('Purged those messages!');
+    }
 };
--- commands/music/e.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'e',
-	description: 'stop playing music',
-	cooldown: 2,
-	execute(message, args, d) {
-        const { currentChannel } = message.client.voice;
-        if (currentChannel) {message.channel.send('e');}
-	}
-};
