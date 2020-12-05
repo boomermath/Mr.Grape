98529daa62ef86dc35f0837a3c4beab54311949f
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-05 13:23:22 -0500

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -8,7 +8,7 @@ module.exports = {
 		if (!channel) return message.channel.send("Go to a voice channel to stop the music!");
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send("There ain't any music!");
-		serverQueue.voiceChannel.leave();
+		message.guild.me.voice.channel.leave();
 		message.client.queue.delete(message.guild.id);
 	}
 };
