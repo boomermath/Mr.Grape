441ca99656720808e59a49c3846bf4cf2e2ed5e6
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-05 16:14:21 -0500

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -4,13 +4,14 @@ module.exports = {
     aliases: ['vol'],
     execute(message, args, d) {
         let title, number;
+        let argument = args.join(' ');
         const { channel } = message.member.voice;
         if (!channel) return message.channel.send('Get in a voice channel if you wanna pump it up!')
         const queue = message.client.queue.get(message.guild.id);
         if (!queue) return message.channel.send('There ain\'t any music!')
-        if (!args) { title = 'Current Volume'; number = queue.volume }
+        if (!argument) { title = 'Current Volume'; number = queue.volume }
         else {
-            let set = parseInt(args.join(' '))
+            let set = parseInt(argument)
             if (set > 10) return message.channel.send("Let's not earrape ppl ok?")
             queue.volume = set;
             queue.connection.dispatcher.setVolumeLogarithmic(set / 5);
