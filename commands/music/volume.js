e1ff410ddd4a1de1bbfbbeaef15f0cb17ce10765
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-05 16:11:14 -0500

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -8,13 +8,15 @@ module.exports = {
         if (!channel) return message.channel.send('Get in a voice channel if you wanna pump it up!')
         const queue = message.client.queue.get(message.guild.id);
         if (!queue) return message.channel.send('There ain\'t any music!')
-        if (!args.join(' ')) { title = 'Current Volume'; number = queue.volume }
-        let set = parseInt(args.join(' '))
-        if (set > 10) return message.channel.send("Let's not earrape ppl ok?")
-        queue.volume = set;
-        queue.connection.dispatcher.setVolumeLogarithmic(set / 5);
-        title = 'Volume set to'
-        number = set
+        if (!args) { title = 'Current Volume'; number = queue.volume }
+        else {
+            let set = parseInt(args.join(' '))
+            if (set > 10) return message.channel.send("Let's not earrape ppl ok?")
+            queue.volume = set;
+            queue.connection.dispatcher.setVolumeLogarithmic(set / 5);
+            title = 'Volume set to'
+            number = set
+        }
         const volumeEmbed = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .addField(title, number)
