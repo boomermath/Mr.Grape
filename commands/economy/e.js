06877070be51beaebefe7077377143722fdcaf8e
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-06 17:42:49 -0500

    Update e.js

--- commands/economy/e.js
+++ commands/economy/e.js
@@ -4,17 +4,17 @@ module.exports = {
     execute(message, args, d) {
         const guild = message.client.guilds.cache.get(message.guild.id);
         const obj = {};
-       guild.members.cache.each(async member => {
+        guild.members.cache.each(async member => {
             let bal = await d.users.get(member.id);
             if (!bal || bal <= 0) { null; }
             else {
                 obj[member.tag] = bal;
             }
         });
-        const lb = Object.entries(lb)
+        const lb = Object.entries(obj)
             .sort(([, a], [, b]) => a - b)
             .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-        
-            message.channel.send(lb)
+
+        message.channel.send(lb)
     }
 };
