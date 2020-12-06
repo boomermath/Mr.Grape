a5081d17868c48e6911037e7096307ebe4c10196
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-06 17:26:27 -0500

    a

---
+++ commands/music/lyrics.js
@@ -0,0 +1,12 @@
+const { kClient } = require('@ksoft/api');
+const ksoft = new KClient(process.env.KSOFT)
+module.exports = {
+    name: 'lyrics',
+    description: 'get lyrics of a song',
+    cooldown: 1,
+    aliases: ['lyr'],
+    async execute(message, args, d) {
+        let e = await ksoft.lyrics.get(args.join(' '));
+        console.log(e);
+    }
+};
