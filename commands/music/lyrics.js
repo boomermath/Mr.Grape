4b7dab06a5c260748e879a1704ce5291afffae9a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-06 18:12:00 -0500

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -6,9 +6,16 @@ module.exports = {
     cooldown: 1,
     aliases: ['lyr'],
     async execute(message, args, d) {
-        const { KSoftClient } = require('@ksoft/api');
-        const ksoft = new KSoftClient(process.env.KSOFT);
-        let e = await ksoft.lyrics.get('Billie Eilish - Bad Guy');
-        console.log(e);
+        const q = message.client.queue.get(message.guild.id);
+        if (!args.length && !q) { return message.channel.send('Give me something to search up bruh') }
+        let argument = args.join(' ')
+        if (!argument) { argument = q.songs[0].title; }
+        let lyrics = await ksoft.lyrics.get(argument);
+        const lyricEmbed = new d.Discord.MessageEmbed()
+            .setColor('#dd2ed0')
+            .setTitle(argument.charAt(0).toUpperCase() + argument.slice(1))
+            .setThumbnail(lyrics.artwork)
+            .addField('Lyrics', lyrics.lyrics)
+        message.channel.send(lyricEmbed);
     }
 };
