3cbfa1242f0bd7630ddbdff2d39286ad004a322a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-06 18:29:38 -0500

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -10,12 +10,22 @@ module.exports = {
         if (!args.length && !q) { return message.channel.send('Give me something to search up bruh') }
         let argument = args.join(' ')
         if (!argument) { argument = q.songs[0].title; }
-        let lyrics = await ksoft.lyrics.get(argument);
+        let res = await ksoft.lyrics.get(argument);
         const lyricEmbed = new d.Discord.MessageEmbed()
             .setColor('#dd2ed0')
             .setTitle(argument.charAt(0).toUpperCase() + argument.slice(1))
-            .setThumbnail(lyrics.artwork)
-            .setDescription('**Lyrics**\n' + lyrics.lyrics)
+            .setThumbnail(res.artwork)
+        if (res.lyrics.length > 2048) {
+            let arr = res.lyrics.match(/(.|[\r\n]){1,n}/g);
+            for (part in arr) {
+                if (part === 0) {
+                    lyricEmbed.addField('**Lyrics**', arr[part]);
+                    continue;
+                }
+                lyricEmbed.addField('\u200b', arr[part]);
+            }
+        }
+        else { lyricEmbed.addField('**Lyrics**', res.lyrics) }
         message.channel.send(lyricEmbed);
     }
 };
