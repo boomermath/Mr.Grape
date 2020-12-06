95725f37fa81e1c8b14ab2a223fbc2511421d6b6
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-06 18:02:15 -0500

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -6,7 +6,9 @@ module.exports = {
     cooldown: 1,
     aliases: ['lyr'],
     async execute(message, args, d) {
-        let { e } = await ksoft.lyrics.get('Billie Eyelash - the bald guy', { textOnly: true });
+        const { KSoftClient } = require('@ksoft/api');
+        const ksoft = new KSoftClient(process.env.KSOFT);
+        let e = await ksoft.lyrics.get('Billie Eilish - Bad Guy');
         console.log(e);
     }
 };
