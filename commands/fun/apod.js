dfafc84cd5367f938ab872016fc305ae24515819
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-08 13:59:45 -0500

    Update apod.js

--- commands/fun/apod.js
+++ commands/fun/apod.js
@@ -1,16 +1,19 @@
 module.exports = {
-    name: 'apod',
-    description: 'get a daily nasapic',
+    name: 'spacepic',
+    description: 'get a nasa pic',
     aliases: ['nasapic', 'npod', 'nasapicoftheday', 'nasa'],
     cooldown: 3,
-    cd: "There's only one NASA pic",
+    cd: "Woah. Chill.",
     async execute(message, args, d) {
-        let NASAURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA}`
-        let pic = await d.r2.get(NASAURL).json;
+        let query = args.join(' ');
+        if (!query) return message.channel.send('You gotta give me something to search up!')
+        let NASAURL = `https://images-api.nasa.gov/search?q=${encodeURIComponent(query)}&media_type=image`
+        let res = await d.r2.get(NASAURL).json;
+        let url = res.collection.items[0].links[0].href;
         const nasa = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle('NASA Picture of the Day!')
-            .setImage(pic.hdurl)
+            .setImage(url)
             .setTimestamp()
             .setFooter('Grape Fortune-Telling');
         message.channel.send(nasa);
