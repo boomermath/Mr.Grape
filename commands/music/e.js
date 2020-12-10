1682b859c65ab22ce44a167d71a78da91052bc74
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-10 11:44:12 -0500

    Update e.js

--- commands/music/e.js
+++ commands/music/e.js
@@ -5,8 +5,9 @@ module.exports = {
     cooldown: 0,
     async execute(message, args, d) {
         let f = await ytdl.getInfo('https://www.youtube.com/watch?v=D42Cexo75a0')
-        let e = await ytdl.getInfo('https://www.youtube.com/watch?v=D42Cexo75a0').videoDetails.thumbnail
-        console.log(e)
         console.log(f)
+        let e = await ytdl.getInfo('https://www.youtube.com/watch?v=D42Cexo75a0').videoDetails
+        console.log(e)
+     
     }
 };
