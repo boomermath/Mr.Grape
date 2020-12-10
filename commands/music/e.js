2ac6ed4935edc4e418795cb46530d7b7c113a337
boomermath <63861512+boomermath@users.noreply.github.com>
2020-12-10 11:49:32 -0500

    Update e.js

--- commands/music/e.js
+++ commands/music/e.js
@@ -4,8 +4,7 @@ module.exports = {
     description: 'get see what song is currently playin',
     cooldown: 0,
     async execute(message, args, d) {
-        let f = await ytdl.getInfo('https://www.youtube.com/watch?v=D42Cexo75a0')
-        console.log(f)
+   
         let e = await ytdl.getInfo('https://www.youtube.com/watch?v=D42Cexo75a0')
         console.log(e.videoDetails)
      
