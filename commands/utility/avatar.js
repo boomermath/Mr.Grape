6720b79dbd609d191ceaa7938e6db08b227d9d97
boomermath <63861512+boomermath@users.noreply.github.com>
2021-03-03 10:09:00 -0500

    avatar cmd!

---
+++ commands/utility/avatar.js
@@ -0,0 +1,28 @@
+const { Command } = require("../../structures");
+const checkmark = "✅"
+
+module.exports =
+    class extends Command {
+        constructor(...args) {
+            super(...args, {
+                name: "avatar",
+                type: "utility",
+                description: "See a person's avatar.",
+                usage: "<optional user>",
+                aliases: ["pfp"],
+                saying: "I just showed it to you!.",
+                cooldown: 2
+            });
+        }
+
+        async main(msg, args) {
+            const avatar = msg.mentions.users.first() || msg.author;
+            const url = avatar.displayAvatarURL().replace(/.jpg$|.png$|.webp$/, "");
+
+            const avatarEmbed = new msg.embed()
+                .setTitle(`${avatar.username}'s avatar`)
+                .setDescription(`[jpg](${url}.jpg) | [png](${url}.png) | [webp](${url}.webp)`)
+                .setImage(avatar.displayAvatarURL())
+            msg.send(avatarEmbed);
+        }
+    };
